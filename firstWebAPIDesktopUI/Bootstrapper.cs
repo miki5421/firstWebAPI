c5cb6bcb735f0afa30d29389bdd5aa350b2d4f21
miki5421 <miki@adamkiewicz.co.uk>
2021-03-21 22:02:38 +0000

    Made few changes to firstWebAPI

---
+++ firstWebAPIDesktopUI/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using firstWebAPIDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace firstWebAPIDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
